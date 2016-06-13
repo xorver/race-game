ab71454f26054b3715edce51a93e5a7f9bab4877
Konrad Zemek <konrad.zemek@gmail.com>
2016-06-14 01:53:57 +0200

    Fix wheels to turn relative to the car.

---
+++ Assets/Scripts/WheelPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelPoint : MonoBehaviour {
+
+
+	private float lastHorizontalInput;
+
+	// Use this for initialization
+	void Start () {
+		lastHorizontalInput = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float hInputDiff = Input.GetAxis ("Horizontal") - lastHorizontalInput;
+		transform.Rotate(new Vector3(0, 45 * hInputDiff, 0));
+		lastHorizontalInput = Input.GetAxis ("Horizontal");
+	}
+}
