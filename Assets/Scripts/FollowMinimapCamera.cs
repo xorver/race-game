185e39fff47363c8ef447d202fd8b61c5b213328
Konrad Zemek <konrad.zemek@gmail.com>
2016-06-14 11:49:55 +0200

    Fix minimap rotations.

--- Assets/Scripts/FollowMinimapCamera.cs
+++ Assets/Scripts/FollowMinimapCamera.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FollowMinimapCamera : MonoBehaviour {
 
 	public Transform target;
+	public Image targetImage;
 	
 	void LateUpdate () {
 		transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+
+		float yRotation = target.transform.eulerAngles.y;
+		var imgTransform = targetImage.transform;
+
+		imgTransform.eulerAngles = new Vector3 (imgTransform.eulerAngles.x, imgTransform.eulerAngles.y, yRotation);
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
 	}
 }
